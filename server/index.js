2a60ce28cf9ca5fb284310c8b81184dfc425be71
Vinston Guillaume <guillauv1@wyvern.acsl.newpaltz.edu>
2019-02-06 23:02:29 -0500

    port exposed

---
+++ server/index.js
@@ -0,0 +1,28 @@
+let mysql  = require('mysql');
+let config = {
+  host    : 'localhost',
+  user    : 'p_s19_2',
+  password: 'zqz5v2',
+  database: 'p_s19_2_db'
+};
+
+
+const express = require('express')
+const app = express()
+const port = 10020
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+let connection = mysql.createConnection(config);
+ 
+// insert statment
+let sql = `INSERT INTO User
+            VALUES('7', 'password', 'admin', 'smith', 'admin@gmail.com', 'admin')`;
+             
+             // execute the insert statment
+             connection.query(sql);
+              
+              connection.end();
